f24e8ac89bd9bcd697bf7e080988bf42f20bf797
edhaarmanniv <edward.haarmann.iv@gmail.com>
2020-06-08 21:58:32 -0500

    addressed all pylint hook messages. Included rc file in order to add linter exceptions

--- resc/gas_station.py
+++ resc/gas_station.py
@@ -1,70 +1,109 @@
+""" Simulator class definition.
+
+Incorporates processes (Car) and resources (Gas Station)
+
+"""
+
 import simpy
-import numpy as np 
+import numpy as np
 
 from scipy.stats import expon
 from scipy.stats import norm
 
-from resc.process_classes import Car
-from resc.resource_classes import GasStation
-from resc.utils import validate_params
+from process_classes import Car
+from resource_classes import GasStation
+from utils import validate_params
 
-class Simulator():
-    
+
+class Simulator:
+    """
+    Models a simulated cars pulling into a gas station with a set number of pumps
+
+    Arguments:
+    ----------
+    params (dict): contains all of the resource, process, and sim time parameters
+    """
+
     def __init__(self, params):
-        
+
         validate_params(params)
-        
+
         self.params = params
         self.env = simpy.Environment()
         self.processes = []
         self.gas_station = None
-        
-    def set_up_resources(self):
-        
-        self.gas_station = GasStation(self.params['num_pumps'], self.env)
-        
-    def schedule_processes(self):
 
-        id = 0
+    def set_up_resources(self):
+        """ Set class attribute to an instance of GasStation
+        """
+        self.gas_station = GasStation(self.params["num_pumps"], self.env)
+
+    def schedule_processes(self):
+        """
+        Simulation Definition.
+
+        Sets watiting time to random exponential between 0 and expected wait time.
+        Initializes parameters to be passed into Car object.
+        Yields Car wait time, creates Car object, appends to Simulator's processes attribute.
+
+        Saves each Car process allows for analysis after running simulation.
+
+        Yields:
+            simpy.Environment.timeout:
+                Time for current process to run. Accumulates until simulation time reached.
+        """
+        car_id = 0
 
         while True:
 
-            waiting_time = expon.rvs(loc=0, scale=self.params['expected_wait'])
+            waiting_time = expon.rvs(loc=0, scale=self.params["expected_wait"])
 
             std_norm = norm.rvs()
-            gas_required = std_norm * self.params['gas_required_std'] + self.params['gas_required_std']
+            gas_required = (
+                std_norm * self.params["gas_required_std"]
+                + self.params["gas_required_std"]
+            )
             gas_required = max([0, gas_required])
 
-            lay_time = expon.rvs(loc=0, scale=self.params['expected_lay_time'])
+            lay_time = expon.rvs(loc=0, scale=self.params["expected_lay_time"])
 
             yield self.env.timeout(waiting_time)
 
             self.processes.append(
                 Car(
-                    id=id,
+                    car_id=car_id,
                     gas_required=gas_required,
                     lay_time=lay_time,
                     gas_station=self.gas_station,
                     env=self.env,
-                    params=self.params
+                    params=self.params,
                 )
             )
 
-            id += 1
-            
-    def run(self, v=0):
+            car_id += 1
+
+    def run(self):
+        """ Method used to run the simulation.
+            "until" set to validated params sim_time
+        """
         self.set_up_resources()
         self.env.process(self.schedule_processes())
-        self.env.run(until=self.params['sim_time']);
-        
-    def report(self):  
-        avg_wait_time = np.array([car.wait_time for car in self.processes if car.finished]).mean()
+        self.env.run(until=self.params["sim_time"])
+
+    def report(self):
+        """ Creates summary of information about Simulator instance after running.
+        """
+        avg_wait_time = np.array(
+            [car.wait_time for car in self.processes if car.finished]
+        ).mean()
 
         total_utilization = self.gas_station.minutes_utilized
-        percent_utilization = total_utilization / (self.params['sim_time'] * self.gas_station.num_pumps)
+        percent_utilization = total_utilization / (
+            self.params["sim_time"] * self.gas_station.num_pumps
+        )
 
         total_gallons_sold = self.gas_station.gallons_sold
 
-        print(f'Average wait time (minutes): {avg_wait_time}')
-        print(f'Percent: {percent_utilization}')
-        print(f'Total Gallons Sold: {total_gallons_sold}')
+        print(f"Average wait time (minutes): {avg_wait_time}")
+        print(f"Percent: {percent_utilization}")
+        print(f"Total Gallons Sold: {total_gallons_sold}")
--- resc/process_classes.py
+++ resc/process_classes.py
@@ -1,69 +1,80 @@
-import numpy as np 
+"""
+Definition of our sole process class, Car.
+"""
 
-class Car():
+
+import numpy as np
+
+
+class Car:
 
     """
     Models a car.
     The car's simulation process in the simulation is the method .run().
-    
+
     Arguments:
     -----------
-    id (int): A unique id for the car
+    car_id (int): A unique id for the car
     gas_required (float): Amount of gallons a car will fill
     lay_time (float): Extra minutes that the car stays at the pump
     gas_station (GasStation): The gas station the cars are going to
     env (simpy.Environment): The simpy environment
-    
+
     """
-    
-    def __init__(
-        self,
-        id,
-        gas_required,
-        lay_time,
-        params,
-        gas_station,
-        env
-    ):
-        
-        self.id = id
+
+    def __init__(self, car_id, gas_required, lay_time, params, gas_station, env):
+
+        self.car_id = car_id
         self.gas_required = gas_required
         self.lay_time = lay_time
         self.gas_station = gas_station
         self.env = env
         self.params = params
-        
+
         self.wait_time = np.inf
         self.finished = False
-        
-        self.action = self.env.process(self.run())
-        
-    def run(self):
-        
-        queue_time = self.env.now
-        
-        print(f'Car {self.id} arrives at {round(queue_time, 2)} minutes')
-                
-        with self.gas_station.resource.request() as req:
-            
-            yield req
-            
-            pump_start_time = self.env.now
-            
-            print(f'Car {self.id} begins utilizing a pump at {round(pump_start_time, 2)} minutes')
 
-            pump_time = self.gas_required / self.params['pump_rate']
+        self.action = self.env.process(self.run())
+
+    def run(self):
+        """
+        Runs through all processes of a Car at a Gas Station.
+        Prints start and end times at pump based on Car's pump rate
+        Assignes key metrics about trip to pump
+
+        Yields:
+            req (simpy.Resource.request): allows for Car's "use" of Gas_Station resource
+            simpy.Environment.timeout:
+                Time for current process to run. Accumulates until simulation time reached.
+        """
+
+        queue_time = self.env.now
+
+        print(f"Car {self.car_id} arrives at {round(queue_time, 2)} minutes")
+
+        with self.gas_station.resource.request() as req:
+
+            yield req
+
+            pump_start_time = self.env.now
+
+            print(
+                f"Car {self.car_id} begins utilizing a pump at {round(pump_start_time, 2)} minutes"
+            )
+
+            pump_time = self.gas_required / self.params["pump_rate"]
             utilization = max(pump_time, self.lay_time)
-            
+
             yield self.env.timeout(utilization)
-            
+
             pump_end_time = self.env.now
-            
-            print(f'Car {self.id} leaves its pump at {round(pump_end_time, 2)} minutes')
-            
+
+            print(
+                f"Car {self.car_id} leaves its pump at {round(pump_end_time, 2)} minutes"
+            )
+
             self.finished = True
-            
+
             self.wait_time = pump_start_time - queue_time
             self.gas_station.minutes_utilized += utilization
             self.gas_station.gallons_sold += self.gas_required
-
